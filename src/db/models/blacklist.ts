320d7419145e3a7baca755d5a1f663a0e47f145b
vrinda-mahajan <vrindamahajan96@gmail.com>
2023-09-09 12:55:22 +0530

    created blacklist schema

---
+++ src/db/models/blacklist.ts
@@ -0,0 +1,31 @@
+import { Document, model, Schema } from "mongoose";
+
+enum BlacklistKind {
+  jti = "jti",
+  token = "token",
+  refresh = "refresh",
+}
+
+const BlacklistSchema = new Schema(
+  {
+    object: {
+      type: String,
+      required: [true, "Please provide an object."],
+      unique: true,
+    },
+    kind: {
+      type: String,
+      enum: ["jti", "token", "refresh"],
+      default: "jti",
+      required: [true, "Please provide a kind."],
+    },
+  },
+  { timestamps: true }
+);
+
+export interface BlacklistDocument extends Document {
+  object: string;
+  kind: string;
+}
+
+export default model<BlacklistDocument>("Blacklist", BlacklistSchema);
